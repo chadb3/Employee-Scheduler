892cbdccb9b5d3137e0b23a0640130c27d863307
chadb3 <chadb3@ksu.edu>
2014-11-20 10:19:59 -0600

    why don't you work like I want

---
+++ EmployeeSchdulerProject/SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿select cast(c.d as date) d, e.first_name, case
+    when r.[date] is null then 'Here'
+	else 'RO'
+	end as present
+from vcalendar c
+     cross join BasicEmployeeData e
+	 left join RequestedDaysOff r
+	 on r.emp_id = e.emp_id
+	 and r.[date] = cast(c.d as date)
+where c.d >= getdate()
+and c.d <= dateadd(month, 1, getdate())
+order by c.d, e.first_name
---
+++ EmployeeSchdulerProject/vcalendar.sql
@@ -0,0 +1,8 @@
+﻿
+create view vcalendar
+as select CAST(D AS DATE) AS D, datename(month, d) as mon, datename(weekday, d) as day_name
+from (select dateadd(day, u.n+10*t.n+100*h.n+1000*th.n, '2014-01-01') as d
+from vten as th
+     cross join vten as h
+	 cross join vten as t
+	 cross join vten as u)X;
